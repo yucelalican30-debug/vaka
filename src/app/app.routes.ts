f071b5eefc2b36215fbfcee6652c1bd8b8a8f2c4
Ali Can Yücel <yucelalican@hotmail.com>
2026-02-01 13:07:27 +0300

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
+
 import { Routes } from '@angular/router';
 import { NotFound } from './components/not-found/not-found';
+import { Layout } from './components/layout/layout';
+import { Device } from './components/device/device';
 
 export const routes: Routes = [
-	// ...other routes
+	{
+		path: '',
+		component: Layout,
+		children: [
+			{ path: '', redirectTo: 'devices', pathMatch: 'full' },
+			{ path: 'devices', component: Device },
+		]
+	},
 	{ path: '**', component: NotFound },
 ];
